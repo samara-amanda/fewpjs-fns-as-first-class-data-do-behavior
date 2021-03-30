767d7d6348053df99ddbad0039ed5a91b2e253f7
Sam <samara.amanda01@gmail.com>
2021-03-29 22:49:43 -0400

    commit tree cc540c86fec3b7e7fcdda5a9ec6c0ab4bd346e82

--- index.js
+++ index.js
@@ -10,4 +10,21 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+function greet(time) {
+  let hour = time.split(":")[0];
+  let greeting = '';
+
+  if (hour < 12) 
+    greeting = "Good Morning"
+  else if (hour > 12 && hour < 17)
+    greeting = "Good Afternoon"
+  else 
+    greeting = "Good Evening"; 
+
+  return greeting 
+}
 /* Write your implementation of displayMessage() */
+function displayMessage(greetingResult){
+  greet = document.getElementById('greeting');
+  greet.innerText = greetingResult; 
+}
